299016a4a61859f278a1d428c35670df7cc2a9ad
root <root@ip-172-31-37-193.ap-northeast-2.compute.internal>
2019-10-25 01:34:05 +0000

    change seed nodes address

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -178,22 +178,10 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        /*
-        vSeeds.emplace_back("seed.bitcoin.sipa.be"); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("dnsseed.bluematt.me"); // Matt Corallo, only supports x9
-        vSeeds.emplace_back("dnsseed.bitcoin.dashjr.org"); // Luke Dashjr
-        vSeeds.emplace_back("seed.bitcoinstats.com"); // Christian Decker, supports x1 - xf
-        vSeeds.emplace_back("seed.bitcoin.jonasschnelli.ch"); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.btc.petertodd.org"); // Peter Todd, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.bitcoin.sprovoost.nl"); // Sjors Provoost
-        vSeeds.emplace_back("dnsseed.emzy.de"); // Stephan Oeste
-        */
 
-        vSeeds.emplace_back("ec2-13-124-151-253.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S1
-        vSeeds.emplace_back("ec2-13-124-208-5.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S2
-        vSeeds.emplace_back("ec2-13-209-9-119.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S3
-        vSeeds.emplace_back("ec2-52-78-205-215.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S4
-        vSeeds.emplace_back("ec2-13-209-85-141.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S5
+        vSeeds.emplace_back("ec2-13-125-143-64.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S1
+        vSeeds.emplace_back("ec2-52-79-229-205.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S2
+        vSeeds.emplace_back("ec2-13-209-12-217.ap-northeast-2.compute.amazonaws.com"); // NDM-TRY2-S3
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
